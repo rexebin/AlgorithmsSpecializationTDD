f86fe6a53e56ddf4e8351150a56ed88ee2f7208b
Rex <rexebin@gmail.com>
2020-11-27 14:02:26 +0000

    Add Find Second Largest Number.

---
+++ DivideAndConquerTDD/Optional Theory Problems (Batch #1)/FindSecondLargestNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace DivideAndConquerTDD
+{
+    public class FindSecondLargestNumber
+    {
+        private int _compareCount;
+
+        public bool IsCompareCountInConstraint(int[] numbers)
+        {
+            //n+log2n-2;
+            var n = numbers.Length;
+            return _compareCount < n + Math.Log2(n) - 2;
+        }
+
+        private int GetLargerNumber(int number1, int number2)
+        {
+            _compareCount++;
+            return number1 > number2 ? number1 : number2;
+        }
+
+        public int GetSecondLargestNumber(int[] numbers)
+        {
+            while (numbers.Length > 2)
+            {
+                var halfLength = numbers.Length / 2;
+                var firstHalf = numbers.Take(halfLength).ToArray();
+                var secondHalf = numbers.Skip(halfLength).Take(numbers.Length - halfLength).ToArray();
+                numbers = GetLargeNumbers(firstHalf, secondHalf);
+            }
+            
+            return numbers[0] > numbers[1] ? numbers[1] : numbers[0];
+        }
+
+        public int[] GetLargeNumbers(int[] numbers1, int[] numbers2)
+        {
+            return numbers1.Select((t, i) => GetLargerNumber(t, numbers2[i])).ToArray();
+        }
+    }
+}
---
+++ DivideAndConquerTDD/Optional Theory Problems (Batch #1)/FindSecondLargestNumberTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+
+namespace DivideAndConquerTDD
+{
+    /**
+     * You are given as input an unsorted array of n distinct numbers, where n is a power of 2.
+     * Give an algorithm that identifies the second-largest number in the array, and that uses
+     * at most n + log_2 n - 2 comparisons.
+     */
+    public class FindSecondLargestNumberTest
+    {
+        private FindSecondLargestNumber sut = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new FindSecondLargestNumber();
+        }
+
+        [Test]
+        [TestCase(new[] {1, 3}, new[] {4, 9}, new[] {4, 9})]
+        [TestCase(new[] {1, 2, 3, 4}, new[] {1, 3, 4, 9}, new[] {1, 3, 4, 9})]
+        [TestCase(new[] {1, 2, 3, 4, 1, 3, 4, 9}, new[] {1, 2, 3, 4, 1, 3, 4, 9}, new[] {1, 2, 3, 4, 1, 3, 4, 9})]
+        public void GivenTwoArrays_ShouldCompareEachPerIndexAndReturnLargeNumbers(int[] numbers1, int[] numbers2,
+            int[] expected)
+        {
+            Assert.AreEqual(expected, sut.GetLargeNumbers(numbers1, numbers2));
+        }
+
+        [Test]
+        [TestCase(new[] {1, 2, 3, 4, 1, 3, 4, 9}, 4)]
+        [TestCase(
+            new[]
+            {
+                1, 3, 82, 23, 4, 5, 65, 2234, 342234, 234, 234, 2234, 2222, 4333, 44334, 344223, 7, 1, 2, 3, 6, 22, 1,
+                1, 1, 1, 1, 1, 1, 1, 1, 1
+            }, 342234)]
+        [TestCase(
+            new[]
+            {
+                3, 12444, 82, 23, 4, 5, 65, 2234, 342234, 234, 234, 2234, 2222, 4333, 44334, 344223, 7, 12, 2, 3, 6, 22,
+                1,
+                12122, 1233, 23, 31, 666, 444, 123, 456, 1254, 123, 44, 82, 23, 4, 5, 65, 2234, 342234, 234, 234, 2234,
+                2222, 4333, 44334,
+                344223, 7, 1113, 2, 3, 6, 22, 11233, 112, 1233, 12333, 11212, 1122, 1112, 33, 123, 123
+            }, 342234)]
+        [TestCase(
+            new[]
+            {
+                3, 12444, 82, 23, 4, 5, 65, 2234, 342234, 234, 234, 2234, 2222, 4333, 44334, 344223, 7, 12, 2, 3, 6, 22,
+                1,
+                12122, 1233, 23, 31, 666, 444, 123, 456, 1254, 123, 44, 82, 23, 4, 5, 65, 2234, 342234, 234, 234, 2234,
+                2222, 4333, 44334,
+                344223, 7, 1113, 2, 3, 6, 22, 11233, 112, 1233, 12333, 11212, 1122, 1112, 33, 123, 123, 3, 12444, 82,
+                23, 4, 5, 65, 2234, 342234, 234, 234, 2234, 2222, 4333, 44334, 344223, 7, 12, 2, 3, 6, 22, 1,
+                12122, 1233, 23, 31, 666, 444, 123, 456, 1254, 123, 44, 82, 23, 4, 5, 65, 2234, 342234, 234, 234, 2234,
+                2222, 4333, 44334,
+                344223, 7, 1113, 2, 3, 6, 22, 11233, 112, 1233, 12333, 11212, 1122, 1112, 33, 123, 123, 3, 12444, 82,
+                23, 4, 5, 65, 2234, 342234, 234, 234, 2234, 2222, 4333, 44334, 344223, 7, 12, 2, 3, 6, 22, 1,
+                12122, 1233, 23, 31, 666, 444, 123, 456, 1254, 123, 44, 82, 23, 4, 5, 65, 2234, 342234, 234, 234, 2234,
+                2222, 4333, 44334,
+                344223, 7, 1113, 2, 3, 6, 22, 11233, 112, 1233, 12333, 11212, 1122, 1112, 33, 123, 123, 3, 12444, 82,
+                23, 4, 5, 65, 2234, 342234, 234, 234, 2234, 2222, 4333, 44334, 344223, 7, 12, 2, 3, 6, 22, 1,
+                12122, 1233, 23, 31, 666, 444, 123, 456, 1254, 123, 44, 82, 23, 4, 5, 65, 2234, 342234, 234, 234, 2234,
+                2222, 4333, 44334,
+                344223, 7, 1113, 2, 3, 6, 22, 11233, 112, 1233, 12333, 11212, 1122, 1112, 33, 123, 123
+            }, 342234)]
+        public void GivenAArrayOfNumbers_ShouldReturnSecondLargestNumber(int[] numbers, int expected)
+        {
+            Assert.AreEqual(expected, sut.GetSecondLargestNumber(numbers));
+            Assert.True(sut.IsCompareCountInConstraint(numbers));
+        }
+    }
+}
