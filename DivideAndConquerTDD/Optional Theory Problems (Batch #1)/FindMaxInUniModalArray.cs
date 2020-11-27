5da2117f177a3ca6c7234e1c5c93b55d983f131c
Rex <rexebin@gmail.com>
2020-11-27 14:47:48 +0000

    Add FindMaxInUniModalArray

---
+++ DivideAndConquerTDD/Optional Theory Problems (Batch #1)/FindMaxInUniModalArray.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace DivideAndConquerTDD
+{
+    public class FindMaxInUniModalArray
+    {
+        public int[] GetPeakPart(int[] numbers)
+        {
+            var halfLength = numbers.Length / 2;
+            return numbers[halfLength - 1] > numbers[halfLength]
+                ? numbers.Take(halfLength).ToArray()
+                : numbers.Skip(halfLength).Take(numbers.Length - halfLength).ToArray();
+        }
+
+        public int GetMax(int[] numbers)
+        {
+            while (numbers.Length > 2)
+            {
+                numbers = GetPeakPart(numbers);
+            }
+            return numbers[0] > numbers[1] ? numbers[0] : numbers[1];
+        }
+    }
+}
---
+++ DivideAndConquerTDD/Optional Theory Problems (Batch #1)/FindMaxInUniModalArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace DivideAndConquerTDD
+{
+    public class FindMaxInUniModalArrayTest
+    {
+        private FindMaxInUniModalArray _sut = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new FindMaxInUniModalArray();
+        }
+
+        [Test]
+        [TestCase(new[] {1, 3, 4, 5, 3, 2, 1}, new[] {5, 3, 2, 1})]
+        [TestCase(new[] {1, 3, 4, 5, 50, 40, 30, 3, 2, 1}, new[] {1, 3, 4, 5, 50})]
+        [TestCase(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4}, new[] {9, 10, 9, 8, 7, 6, 5, 4})]
+        public void GivenAnArray_ShouldReturnPeakHalf(int[] numbers, int[] expected)
+        {
+            Assert.AreEqual(expected, _sut.GetPeakPart(numbers));
+        }
+
+        [Test]
+        [TestCase(new[] {1, 3, 4, 5, 3, 2, 1}, 5)]
+        [TestCase(new[] {1, 3, 4, 5, 50, 40, 30, 3, 2, 1}, 50)]
+        [TestCase(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4}, 10)]
+        public void GivenAnArray_ShouldReturnMax(int[] numbers, int expected)
+        {
+            Assert.AreEqual(expected, _sut.GetMax(numbers));
+        }
+    }
+}
