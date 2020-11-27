36b887dca519ed3fa2c533d21e178e09d1f26036
Rex <rexebin@gmail.com>
2020-11-27 14:03:41 +0000

    Add Find Second Largest Number.

--- DivideAndConquerTDD/Optional Theory Problems (Batch #1)/FindSecondLargestNumber.cs
+++ DivideAndConquerTDD/Optional Theory Problems (Batch #1)/FindSecondLargestNumber.cs
@@ -7,18 +7,6 @@ namespace DivideAndConquerTDD
     {
         private int _compareCount;
 
-        public bool IsCompareCountInConstraint(int[] numbers)
-        {
-            //n+log2n-2;
-            var n = numbers.Length;
-            return _compareCount < n + Math.Log2(n) - 2;
-        }
-
-        private int GetLargerNumber(int number1, int number2)
-        {
-            _compareCount++;
-            return number1 > number2 ? number1 : number2;
-        }
 
         public int GetSecondLargestNumber(int[] numbers)
         {
@@ -29,13 +17,26 @@ namespace DivideAndConquerTDD
                 var secondHalf = numbers.Skip(halfLength).Take(numbers.Length - halfLength).ToArray();
                 numbers = GetLargeNumbers(firstHalf, secondHalf);
             }
-            
+
             return numbers[0] > numbers[1] ? numbers[1] : numbers[0];
         }
 
         public int[] GetLargeNumbers(int[] numbers1, int[] numbers2)
         {
             return numbers1.Select((t, i) => GetLargerNumber(t, numbers2[i])).ToArray();
+        }
+
+        private int GetLargerNumber(int number1, int number2)
+        {
+            _compareCount++;
+            return number1 > number2 ? number1 : number2;
+        }
+
+        public bool IsCompareCountInConstraint(int[] numbers)
+        {
+            //n+log2n-2;
+            var n = numbers.Length;
+            return _compareCount < n + Math.Log2(n) - 2;
         }
     }
 }
