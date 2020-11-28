0592f1e75f845efa14d5cffead097ee3473a8188
Rex Ye <rexebin@gmail.com>
2020-11-28 00:06:50 +0000

    Add Local Minimum

--- DivideAndConquerTDD/Optional Theory Problems (Batch #1)/LocalMinimum.cs
+++ DivideAndConquerTDD/Optional Theory Problems (Batch #1)/LocalMinimum.cs
@@ -1,10 +1,51 @@
-﻿namespace DivideAndConquerTDD
+﻿using System;
+
+namespace DivideAndConquerTDD
 {
+    public record Dimension(int Top, int Bottom, int Left, int Right, int VCut, int lastHIndex);
+
     public class LocalMinimum
     {
-        public int GetLocalMinimum(int[][] input)
+        public double GetLocalMinimum(int[][] input, Dimension range)
         {
-            return 1;
+            var hIndex = GetHIndex(input, range);
+            var vIndex = GetVIndex(input, range, hIndex);
+            if (vIndex == range.VCut)
+                return input[range.VCut][hIndex];
+
+            range = ResetHorizontalBoundary(range, hIndex);
+            range = ResetVerticalBoundary(range, vIndex);
+            range = range with{VCut = vIndex};
+            return GetLocalMinimum(input, range);
+        }
+
+        private static Dimension ResetVerticalBoundary(Dimension range, int vIndex)
+        {
+            return vIndex > range.VCut ? range with {Top = range.VCut + 1} : range with {Bottom = range.VCut - 1};
+        }
+
+        private static Dimension ResetHorizontalBoundary(Dimension range, int hIndex)
+        {
+            if (range.lastHIndex <= 0)
+                return range with{lastHIndex = hIndex};
+
+            return hIndex > range.lastHIndex ? range with{Left = hIndex + 1} : range with{Right = hIndex - 1};
+        }
+
+        private int GetVIndex(int[][] input, Dimension range, int hIndex)
+        {
+            var vIndex = range.Top;
+            for (int i = range.Top + 1; i <= range.Bottom; i++)
+                vIndex = input[vIndex][hIndex] > input[i][hIndex] ? i : vIndex;
+            return vIndex;
+        }
+
+        private int GetHIndex(int[][] input, Dimension range)
+        {
+            var hIndex = range.Left;
+            for (int i = hIndex + 1; i <= range.Right; i++)
+                hIndex = input[range.VCut][hIndex] > input[range.VCut][i] ? i : hIndex;
+            return hIndex;
         }
     }
 }
--- DivideAndConquerTDD/Optional Theory Problems (Batch #1)/LocalMinimumTest.cs
+++ DivideAndConquerTDD/Optional Theory Problems (Batch #1)/LocalMinimumTest.cs
@@ -8,23 +8,85 @@ namespace DivideAndConquerTDD
      * neighbors. (A neighbor of a number is one immediately above, below, to the left, or the right. Most numbers have
      * four neighbors; numbers on the side have three; the four corners have two.) Use the divide-and-conquer algorithm
      * design paradigm to compute a local minimum with only O(n) comparisons between pairs of numbers. (Note: since
-     * there are n^2 numbers in the input, you cannot afford to look at all of them. Hint: Think about what types
-     * of recurrences would give you the desired upper bound.)
+     * there are n^2 numbers in the input, you cannot afford to look at all of them.
+     * Hint: Think about what types of recurrences would give you the desired upper bound.)
      */
     public class LocalMinimumTest
     {
         [Test]
-        public void GivenANByNArray_ShouldReturnALocalMinimum()
+        public void GivenANAndNArrayAndRange_ShouldReturnLocalMinWhenCrossed()
         {
-            var input = new int[][]
+            var n = 3;
+            var range = new Dimension(0, n - 1, 0, n - 1, (n - 1) / 2, -1);
+            var input = new[]
             {
-                new[] {1, 2},
-                new[] {3, 4}
+                new[] {4, 6, 2},
+                new[] {3, 4, 5},
+                new[] {5, 6, 7}
             };
+            var expected = 3;
             var sut = new LocalMinimum();
-            var expected = 1;
-            Assert.AreEqual(expected, sut.GetLocalMinimum(input));
+            var result = sut.GetLocalMinimum(input, range);
+            Assert.AreEqual(expected, result);
         }
-        
+
+        [Test]
+        public void GivenANAndNArrayAndRange_ShouldReturnLocalMin()
+        {
+            var n = 3;
+            var range = new Dimension(0, n - 1, 0, n - 1, n / 2, -1);
+            var input = new[]
+            {
+                new[] {4, 6, 2},
+                new[] {8, 3, 9},
+                new[] {1, -1, 5}
+            };
+            var expected = -1;
+            var sut = new LocalMinimum();
+            var result = sut.GetLocalMinimum(input, range);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenANAndNArrayAndRange_ShouldReturnLocalMin1()
+        {
+            var n = 4;
+            var range = new Dimension(0, n - 1, 0, n - 1, n / 2 - 1, -1);
+            var input = new[]
+            {
+                new[] {4, 6, 2, 10},
+                new[] {-10, 3, 9, 15},
+                new[] {-11, -80, -70, -60},
+                new[] {-19, -30, -40, -50}
+            };
+            var expected = -80;
+            var sut = new LocalMinimum();
+            var result = sut.GetLocalMinimum(input, range);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenANAndNArrayAndRange_ShouldReturnLocalMin2()
+        {
+            var n = 10;
+            var range = new Dimension(0, n - 1, 0, n - 1, n / 2 - 1, -1);
+            var input = new[]
+            {
+                new[] {55, 45, 38, 23, 88, 46, 7, 89, 0, 94},
+                new[] {2, 92, 43, 51, 58, 67, 82, 90, 79, 17},
+                new[] {29, 64, 16, 8, 50, 14, 1, 25, 26, 73},
+                new[] {97, 37, 13, 20, 4, 75, 98, 80, 48, 12},
+                new[] {33, 27, 42, 74, 95, 35, 57, 53, 96, 60},
+                new[] {59, 86, 76, 40, 6, 11, 77, 49, 93, 61},
+                new[] {5, 72, 9, 91, 68, 30, 39, 69, 99, 21},
+                new[] {52, 31, 28, 34, 3, 81, 18, 62, 10, 71},
+                new[] {66, 24, 44, 54, 56, 85, 84, 22, 47, 63},
+                new[] {65, 36, 83, 41, 15, 19, 87, 78, 70, 32},
+            };
+            var expected = 22;
+            var sut = new LocalMinimum();
+            var result = sut.GetLocalMinimum(input, range);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
