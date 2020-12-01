2170ce9132f46bbe60b2f084acb269d575470b9f
Rex <rexebin@gmail.com>
2020-12-01 16:23:23 +0000

    Add Assignment 2#

---
+++ DivideAndConquerTDD/AssignmentTwo/CountingInversions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DivideAndConquerTDD.AssignmentTwo
+{
+    public class CountingInversions
+    {
+        private long _inversions;
+
+        public int[] SortAndCountInversions(int[] input)
+        {
+            if (input.Length == 1)
+                return input;
+
+            var length = input.Length;
+            var left = SortAndCountInversions(input.Take(length / 2).ToArray());
+            var right = SortAndCountInversions(input.Skip(length / 2).Take(length - length / 2).ToArray());
+            return MergeAndSort(left, right);
+        }
+
+        public int[] SortAndCountInversions()
+        {
+            var input = File.ReadAllLines(GetPath())
+                .Select(int.Parse).ToArray();
+
+            if (input.Length == 1)
+                return input;
+
+            var length = input.Length;
+            var left = SortAndCountInversions(input.Take(length / 2).ToArray());
+            var right = SortAndCountInversions(input.Skip(length / 2).Take(length - length / 2).ToArray());
+            return MergeAndSort(left, right);
+        }
+
+        public long GetInversions()
+        {
+            return _inversions;
+        }
+
+        public int[] MergeAndSort(int[] arrayB, int[] arrayC)
+        {
+            var mergedArray = new List<int>();
+            var j = 0;
+            var i = 0;
+            while (i < arrayB.Length)
+            {
+                if (j == arrayC.Length)
+                {
+                    mergedArray.Add(arrayB[i]);
+                    i++;
+                    continue;
+                }
+
+                if (arrayB[i] < arrayC[j])
+                {
+                    mergedArray.Add(arrayB[i]);
+                    i++;
+                }
+                else
+                {
+                    mergedArray.Add(arrayC[j]);
+                    _inversions += arrayB.Length - i;
+                    j++;
+                }
+            }
+
+            while (j < arrayC.Length)
+            {
+                mergedArray.Add(arrayC[j]);
+                j++;
+            }
+
+
+            return mergedArray.ToArray();
+        }
+
+        public string GetPath()
+        {
+            return Path.GetFullPath(@"..\..\..\AssignmentTwo\IntegerArray.txt");
+        }
+    }
+}
---
+++ DivideAndConquerTDD/AssignmentTwo/CountingInversionsTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+namespace DivideAndConquerTDD.AssignmentTwo
+{
+    public class CountingInversionsTest
+    {
+        [Test]
+        [TestCase(new[] {1, 3, 5}, new[] {2, 4, 6}, new[] {1, 2, 3, 4, 5, 6}, 3)]
+        public void GivenTwoSortedArrays_ShouldMergeSortAndCountInversions(int[] arrayB, int[] arrayC,
+            int[] mergedArray, int inversions)
+        {
+            var sut = new CountingInversions();
+            var result = sut.MergeAndSort(arrayB, arrayC);
+            var inversionResult = sut.GetInversions();
+            Assert.AreEqual(mergedArray, result);
+            Assert.AreEqual(inversions, inversionResult);
+        }
+
+
+        [Test]
+        [TestCase(new[] {1}, new[] {1}, 0)]
+        [TestCase(new[] {2, 1}, new[] {1, 2}, 1)]
+        [TestCase(new[] {1, 2}, new[] {1, 2}, 0)]
+        [TestCase(new[] {1, 3, 5, 2, 4, 6}, new[] {1, 2, 3, 4, 5, 6}, 3)]
+        [TestCase(new[] {6, 5, 4, 3, 2, 1}, new[] {1, 2, 3, 4, 5, 6}, 15)]
+        [TestCase(new[] {6, 5, 4, 3, 2, 1, 0}, new[] {0, 1, 2, 3, 4, 5, 6}, 21)]
+        public void GivenArray_ShouldReturnSortedArrayAndCountInversions(int[] input, int[] sortedArray, int inversions)
+        {
+            var sut = new CountingInversions();
+            var result = sut.SortAndCountInversions(input);
+            var inversionsResult = sut.GetInversions();
+            Assert.AreEqual(sortedArray, result);
+            Assert.AreEqual(inversions, inversionsResult);
+        }
+
+        [Test]
+        public void ShouldGetIntegerArrayPath()
+        {
+            var sut = new CountingInversions();
+            var path = sut.GetPath();
+            Assert.True(path.Contains("DivideAndConquerTDD\\DivideAndConquerTDD\\AssignmentTwo\\IntegerArray.txt"));
+        }
+
+        [Test]
+        public void GivenTextFile_ShouldReturnSortedArrayAndCountInversions()
+        {
+            var sut = new CountingInversions();
+            var result = sut.SortAndCountInversions();
+            var inversionsResult = sut.GetInversions();
+            Assert.AreEqual(2407905288, inversionsResult);
+        }
+    }
+}
