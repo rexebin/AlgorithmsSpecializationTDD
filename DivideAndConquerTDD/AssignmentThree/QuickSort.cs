cca42664d7c7fe6388db747d9153b2a7b2715cb3
Rex <rexebin@gmail.com>
2020-12-02 14:54:51 +0000

    Add Assignment 3#

---
+++ DivideAndConquerTDD/AssignmentThree/QuickSort.cs
@@ -0,0 +1,120 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Xsl;
+
+namespace DivideAndConquerTDD.AssignmentThree
+{
+    public enum PivotMode
+    {
+        FirstElement,
+        LastElement,
+        Median
+    }
+
+    public class QuickSort
+    {
+        private long _count;
+
+        public int[] Sort(int[] input, int left, int right, PivotMode mode = PivotMode.FirstElement)
+        {
+            _count += right - left - 1;
+            input = SwapPivotToFirst(input, left, right, mode);
+            var splitIndex = PartitionAndReturnSplitIndex(input, left, right);
+            if (splitIndex - 1 > left)
+                Sort(input, left, splitIndex - 1, mode);
+
+            if (splitIndex < right - 1)
+                Sort(input, splitIndex, right, mode);
+
+            return input;
+        }
+
+        private int PartitionAndReturnSplitIndex(int[] input, int left, int right)
+        {
+            var pivot = input[left];
+            var splitIndex = left + 1;
+            for (int index = left + 1; index < right; index++)
+            {
+                if (input[index] > pivot)
+                    continue;
+                if (index != splitIndex)
+                {
+                    var oldI = input[splitIndex];
+                    input[splitIndex] = input[index];
+                    input[index] = oldI;
+                }
+
+                splitIndex++;
+            }
+
+            input[left] = input[splitIndex - 1];
+            input[splitIndex - 1] = pivot;
+            return splitIndex;
+        }
+
+        private int[] SwapPivotToFirst(int[] input, int left, int right, PivotMode mode)
+        {
+            return mode switch
+            {
+                PivotMode.LastElement => SwapLastAndFirstElement(input, left, right),
+                PivotMode.Median => SwapMedianWithFirstElement(input, left, right),
+                _ => input
+            };
+        }
+
+        public long Sort(PivotMode mode = PivotMode.FirstElement)
+        {
+            var input = File.ReadAllLines(GetPath())
+                .Select(int.Parse).ToArray();
+            var sorted = Sort(input, 0, input.Length, mode);
+            return _count;
+        }
+
+        private string GetPath()
+        {
+            return Path.GetFullPath(@"..\..\..\AssignmentThree\QuickSort.txt");
+        }
+
+        public long GetCount()
+        {
+            return _count;
+        }
+
+        public int[] SwapLastAndFirstElement(int[] input, int left, int right)
+        {
+            var lastElement = input[right - 1];
+            input[right - 1] = input[left];
+            input[left] = lastElement;
+            return input;
+        }
+
+        public int[] SwapMedianWithFirstElement(int[] input, int left, int right)
+        {
+            var medianIndex = GetMedianIndex(input, left, right);
+            var firstElement = input[left];
+            input[left] = input[medianIndex];
+            input[medianIndex] = firstElement;
+            return input;
+        }
+
+        public int GetMiddleIndex(int[] input)
+        {
+            return input.Length / 2 - (1 - input.Length % 2);
+        }
+
+        public int GetMedianIndex(int[] input, int left, int right)
+        {
+            var medianIndex = GetMiddleIndex(input);
+            if (input[left] > input[medianIndex] && input[left] < input[right - 1] ||
+                input[left] > input[right - 1] && input[left] < input[medianIndex])
+                return left;
+
+            if (input[medianIndex] > input[left] && input[medianIndex] < input[right - 1] ||
+                input[medianIndex] > input[right - 1] && input[medianIndex] < input[left]
+            )
+                return medianIndex;
+
+            return right - 1;
+        }
+    }
+}
---
+++ DivideAndConquerTDD/AssignmentThree/QuickSortTest.cs
@@ -0,0 +1,139 @@
+﻿using NUnit.Framework;
+
+namespace DivideAndConquerTDD.AssignmentThree
+{
+    public class QuickSortTest
+    {
+        [Test]
+        [TestCase(new[] {3, 8, 2, 5, 1, 4, 7, 6}, new[] {1, 2, 3, 4, 5, 6, 7, 8}, 15)]
+        [TestCase(new[] {3, 2, 8, 5, 1, 4, 7, 6}, new[] {1, 2, 3, 4, 5, 6, 7, 8}, 15)]
+        [TestCase(new[] {3, 2, 8, 5, 1, 4, 7, 6, 15, 14, 13, 11, 10, 9, 12},
+            new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, 51)]
+        public void GivenArray_ShouldUseFirstElementAsPivot_ReturnPartitionedArrays(int[] input, int[] expected,
+            int count)
+        {
+            var sut = new QuickSort();
+            var result = sut.Sort(input, 0, input.Length);
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(count, sut.GetCount());
+        }
+
+        [Test]
+        public void ShouldReadArrayFromText_UseFirstElementAsPivot_SortAndReturnCompareCount()
+        {
+            var sut = new QuickSort();
+            var count = sut.Sort();
+            Assert.AreEqual(162085, count);
+        }
+
+        [Test]
+        [TestCase(new[] {3, 8, 2, 5, 1, 4, 7, 6}, new[] {1, 2, 3, 4, 5, 6, 7, 8}, 15)]
+        [TestCase(new[] {3, 2, 8, 5, 1, 4, 7, 6}, new[] {1, 2, 3, 4, 5, 6, 7, 8}, 15)]
+        [TestCase(new[] {3, 2, 8, 5, 1, 4, 7, 6, 15, 14, 13, 11, 10, 9, 12},
+            new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, 43)]
+        public void GivenArray_ShouldUseLastElementAsPivot_ReturnPartitionedArrays(int[] input, int[] expected,
+            int count)
+        {
+            var sut = new QuickSort();
+            var result = sut.Sort(input, 0, input.Length, PivotMode.LastElement);
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(count, sut.GetCount());
+        }
+
+        [Test]
+        public void ShouldSwapFirstAndLastElement()
+        {
+            var sut = new QuickSort();
+            var input = sut.SwapLastAndFirstElement(new[] {1, 2, 3, 4, 5}, 0, 5);
+            Assert.AreEqual(new int[] {5, 2, 3, 4, 1}, input);
+        }
+
+
+        [Test]
+        public void ShouldReadArrayFromText_UseLastElementAsPivot_SortAndReturnCompareCount()
+        {
+            var sut = new QuickSort();
+            var count = sut.Sort(PivotMode.LastElement);
+            Assert.AreEqual(164123, count);
+        }
+
+        [Test]
+        [TestCase(new[] {4, 5, 6, 7}, 1)]
+        [TestCase(new[] {4, 5, 6, 7, 8}, 2)]
+        public void GivenArray4567_ShouldReturnMiddleIndex(int[] input, int expected)
+        {
+            var sut = new QuickSort();
+            var median = sut.GetMiddleIndex(input);
+            Assert.AreEqual(expected, median);
+        }
+
+        [Test]
+        [TestCase(new[] {4, 5, 6, 7}, 0, 4, 1)]
+        [TestCase(new[] {4, 5, 3, 7, 8}, 0, 5, 0)]
+        [TestCase(new[] {4, 5, 2, 7, 3}, 0, 5, 4)]
+        [TestCase(new[] {9, 2, 4, 6, 7, 8, 9, 3, 4}, 0, 9, 4)]
+        [TestCase(new[] {4, 2, 4, 6, 7, 8, 9, 3, 9}, 0, 9, 4)]
+        [TestCase(new[] {9, 2, 4, 6, 4, 8, 9, 3, 7}, 0, 9, 8)]
+        [TestCase(new[] {4, 2, 4, 6, 9, 8, 9, 3, 7}, 0, 9, 8)]
+        [TestCase(new[] {7, 2, 4, 6, 4, 8, 9, 3, 9}, 0, 9, 0)]
+        [TestCase(new[] {7, 2, 4, 6, 9, 8, 9, 3, 4}, 0, 9, 0)]
+        [TestCase(new[] {9, 2, 4, 6, 4, 8, 3, 7}, 0, 8, 7)]
+        [TestCase(new[] {9, 2, 4, 6, 9, 8, 3, 7}, 0, 8, 7)]
+        [TestCase(new[] {9, 2, 4, 7, 4, 8, 3, 6}, 0, 8, 3)]
+        [TestCase(new[] {6, 2, 4, 7, 4, 8, 3, 9}, 0, 8, 3)]
+        [TestCase(new[] {7, 2, 4, 9, 4, 8, 3, 6}, 0, 8, 0)]
+        [TestCase(new[] {7, 2, 4, 6, 4, 8, 3, 9}, 0, 8, 0)]
+        public void GivenArray4567_ShouldCompareAndReturnMedianIndex(int[] input, int left, int right, int expected)
+        {
+            var sut = new QuickSort();
+            var median = sut.GetMedianIndex(input, left, right);
+            Assert.AreEqual(expected, median);
+        }
+
+
+        [Test]
+        [TestCase(new[] {1, 2, 3, 4, 5}, 0, 5, new int[] {3, 2, 1, 4, 5})]
+        [TestCase(new[] {1, 2, 3, 4}, 0, 4, new int[] {2, 1, 3, 4})]
+        [TestCase(new[] {9, 2, 4, 6, 7, 8, 9, 3, 4}, 0, 9, new int[] {7, 2, 4, 6, 9, 8, 9, 3, 4})]
+        [TestCase(new[] {4, 2, 4, 6, 7, 8, 9, 3, 9}, 0, 9, new []{7, 2, 4, 6, 4, 8, 9, 3, 9})]
+        [TestCase(new[] {9, 2, 4, 6, 4, 8, 9, 3, 7}, 0, 9, new []{7, 2, 4, 6, 4, 8, 9, 3, 9})]
+        [TestCase(new[] {4, 2, 4, 6, 9, 8, 9, 3, 7}, 0, 9, new []{7, 2, 4, 6, 9, 8, 9, 3, 4})]
+        [TestCase(new[] {7, 2, 4, 6, 4, 8, 9, 3, 9}, 0, 9, new []{7, 2, 4, 6, 4, 8, 9, 3, 9})]
+        [TestCase(new[] {7, 2, 4, 6, 9, 8, 9, 3, 4}, 0, 9, new []{7, 2, 4, 6, 9, 8, 9, 3, 4})]
+        [TestCase(new[] {9, 2, 4, 6, 4, 8, 3, 7}, 0, 8, new []{7, 2, 4, 6, 4, 8, 3, 9})]
+        [TestCase(new[] {9, 2, 4, 6, 4, 8, 3, 7}, 0, 8, new []{7, 2, 4, 6, 4, 8, 3, 9})]
+        [TestCase(new[] {9, 2, 4, 7, 4, 8, 3, 6}, 0, 8, new []{7, 2, 4, 9, 4, 8, 3, 6})]
+        [TestCase(new[] {6, 2, 4, 7, 4, 8, 3, 9}, 0, 8, new []{7, 2, 4, 6, 4, 8, 3, 9})]
+        [TestCase(new[] {7, 2, 4, 9, 4, 8, 3, 6}, 0, 8, new []{7, 2, 4, 9, 4, 8, 3, 6})]
+        [TestCase(new[] {7, 2, 4, 6, 4, 8, 3, 9}, 0, 8, new []{7, 2, 4, 6, 4, 8, 3, 9})]
+        public void ShouldSwapMedianAndFirstElement(int[] input, int left, int right, int[] expected)
+        {
+            var sut = new QuickSort();
+            var result = sut.SwapMedianWithFirstElement(input, left, right);
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [Test]
+        [TestCase(new[] {3, 8, 2, 5, 1, 4, 7, 6}, new[] {1, 2, 3, 4, 5, 6, 7, 8}, 16)]
+        [TestCase(new[] {3, 2, 8, 5, 1, 4, 7, 6}, new[] {1, 2, 3, 4, 5, 6, 7, 8}, 16)]
+        [TestCase(new[] {3, 2, 8, 5, 1, 4, 7, 6, 15, 14, 13, 11, 10, 9, 12},
+            new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, 41)]
+        public void GivenArray_ShouldMedianAsPivot_ReturnPartitionedArrays(int[] input, int[] expected,
+            int count)
+        {
+            var sut = new QuickSort();
+            var result = sut.Sort(input, 0, input.Length, PivotMode.Median);
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(count, sut.GetCount());
+        }
+
+        [Test]
+        public void ShouldReadArrayFromText_UseMedianElementAsPivot_SortAndReturnCompareCount()
+        {
+            var sut = new QuickSort();
+            var count = sut.Sort(PivotMode.Median);
+            Assert.AreEqual(156383, count);
+        }
+    }
+}
