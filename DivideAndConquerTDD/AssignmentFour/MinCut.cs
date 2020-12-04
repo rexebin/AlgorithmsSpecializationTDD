f57c30616cf2828ca94cb0afdefa89bcd04215df
Rex <rexebin@gmail.com>
2020-12-04 11:43:17 +0000

    Add Assignment 4#

---
+++ DivideAndConquerTDD/AssignmentFour/MinCut.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DivideAndConquerTDD.AssignmentFour
+{
+    public class MinCut
+    {
+        private Dictionary<int, List<int>> _graph;
+
+        public MinCut(Dictionary<int, List<int>> graph)
+        {
+            _graph = graph;
+        }
+
+        public int GetRandomKeyVertex()
+        {
+            return _graph.Keys.ToList()[new Random(Guid.NewGuid().GetHashCode()).Next(0, _graph.Count)];
+        }
+
+        public int GetRandomTargetVertex(int key)
+        {
+            var adjacentVertices = GetValue(key);
+            return adjacentVertices[new Random(Guid.NewGuid().GetHashCode()).Next(0, adjacentVertices.Count)];
+        }
+
+        public void ReplaceKeyVertexWithTargetVertex(int key, int target)
+        {
+            var keyVertexValues = GetValue(key);
+            foreach (var keyVertexValue in keyVertexValues)
+            {
+                var values = GetValue(keyVertexValue);
+                values = values.Select(x => x == key ? target : x).ToList();
+                _graph[keyVertexValue] = values;
+            }
+        }
+
+        private List<int> GetValue(int key)
+        {
+            _graph.TryGetValue(key, out var adjacentVertices);
+            if (adjacentVertices == null) throw new Exception("key is not valid");
+            return adjacentVertices;
+        }
+
+        public Dictionary<int, List<int>> GetGraph()
+        {
+            return _graph;
+        }
+
+        public void RemoveVertex(int key)
+        {
+            _graph.Remove(key);
+        }
+
+        public void MergeKeyValuesToTargetValues(int key, int target)
+        {
+            var targetValues = GetValue(target);
+            var keyValues = GetValue(key);
+            targetValues.AddRange(keyValues);
+            _graph[target] = targetValues;
+        }
+
+        public void RemoveSelfLoop(int key)
+        {
+            var values = GetValue(key);
+            values = values.Where(x => x != key).ToList();
+            _graph[key] = values;
+        }
+
+        public int GetMinCut()
+        {
+            var minCut = int.MaxValue;
+            var originalGraph = CloneDictionary(_graph);
+            for (int i = 0; i <= originalGraph.Count; i++)
+            {
+                _graph = CloneDictionary(originalGraph);
+                var result = GetCutResult();
+                minCut = minCut > result ? result : minCut;
+            }
+
+            return minCut;
+        }
+
+        private int GetCutResult()
+        {
+            while (_graph.Count > 2)
+            {
+                var vertexToMerge = GetRandomKeyVertex();
+                var targetMergeVertex = GetRandomTargetVertex(vertexToMerge);
+                MergeKeyValuesToTargetValues(vertexToMerge, targetMergeVertex);
+                ReplaceKeyVertexWithTargetVertex(vertexToMerge, targetMergeVertex);
+                RemoveVertex(vertexToMerge);
+                RemoveSelfLoop(targetMergeVertex);
+            }
+
+            return _graph.First().Value.Count;
+        }
+
+        private Dictionary<int, List<int>> CloneDictionary(Dictionary<int, List<int>> graph)
+        {
+            return graph.ToDictionary(entry => entry.Key,
+                entry => entry.Value.ToList());
+        }
+
+        public static Dictionary<int, List<int>> ReadFile()
+        {
+            var list = File.ReadAllLines(GetPath())
+                .Select(x => x.TrimEnd().Split('\t').ToList());
+            return list.ToDictionary(entry => int.Parse(entry.First()),
+                entry => entry.Skip(1).Take(entry.Count).Select(x => int.Parse(x)).ToList());
+        }
+
+        private static string GetPath()
+        {
+            return Path.GetFullPath(@"..\..\..\AssignmentFour\kargerMinCut.txt");
+        }
+    }
+}
---
+++ DivideAndConquerTDD/AssignmentFour/MinCutTest.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace DivideAndConquerTDD.AssignmentFour
+{
+    public class MinCutTest
+    {
+        [Test]
+        public void ShouldGetRandomKeyVertex()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2, 3}},
+                {2, new List<int> {1, 3}},
+                {3, new List<int> {1, 2}}
+            };
+            var sut = new MinCut(graph);
+            var result = sut.GetRandomKeyVertex();
+            Assert.Contains(result, new[] {1, 2, 3});
+        }
+
+        [Test]
+        public void ShouldGetRandomTargetVertex()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2, 3, 4, 5, 6, 7, 8, 9}}
+            };
+            var sut = new MinCut(graph);
+            var result = sut.GetRandomTargetVertex(1);
+            Assert.Contains(result, new[] {2, 3, 4, 5, 6, 7, 8, 9});
+        }
+
+        [Test]
+        public void ShouldMergeKeyValuesToTargetValues()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2, 3}},
+                {2, new List<int> {1, 3}},
+                {3, new List<int> {1, 2}}
+            };
+            var sut = new MinCut(graph);
+            sut.MergeKeyValuesToTargetValues(1, 2);
+            var result = sut.GetGraph();
+            var expected = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2, 3}},
+                {2, new List<int> {1, 3, 2, 3}},
+                {3, new List<int> {1, 2}}
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ShouldReplaceKeyVertex1WithTargetVertex2()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2, 3}},
+                {2, new List<int> {1, 3, 2, 3}},
+                {3, new List<int> {1, 2}}
+            };
+            var sut = new MinCut(graph);
+            sut.ReplaceKeyVertexWithTargetVertex(1, 2);
+            var result = sut.GetGraph();
+            var expected = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2, 3}},
+                {2, new List<int> {2, 3, 2, 3}},
+                {3, new List<int> {2, 2}}
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [Test]
+        public void ShouldRemoveKeyVertex()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2, 3}},
+                {2, new List<int> {2, 3, 2, 3}},
+                {3, new List<int> {2, 2}}
+            };
+            var sut = new MinCut(graph);
+            sut.RemoveVertex(1);
+            var result = sut.GetGraph();
+            var expected = new Dictionary<int, List<int>>
+            {
+                {2, new List<int> {2, 3, 2, 3}},
+                {3, new List<int> {2, 2}}
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ShouldRemoveLeftLoop()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {2, new List<int> {2, 3, 2, 3}},
+                {3, new List<int> {2, 2}}
+            };
+            var sut = new MinCut(graph);
+            sut.RemoveSelfLoop(2);
+            var result = sut.GetGraph();
+            var expected = new Dictionary<int, List<int>>
+            {
+                {2, new List<int> {3, 3}},
+                {3, new List<int> {2, 2}}
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ShouldReturnMinCut()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {2, new List<int> {3, 3}},
+                {3, new List<int> {2, 2}}
+            };
+            var sut = new MinCut(graph);
+            var result = sut.GetMinCut();
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void GivenTwoVerticesGraph_ShouldCalculateMinCut()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2}},
+                {2, new List<int> {1}}
+            };
+            var sut = new MinCut(graph);
+            var result = sut.GetMinCut();
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void GivenFourVerticesGraph_ShouldCalculateMinCut()
+        {
+            var graph = new Dictionary<int, List<int>>
+            {
+                {1, new List<int> {2, 4}},
+                {2, new List<int> {1, 3, 4}},
+                {3, new List<int> {2, 4}},
+                {4, new List<int> {1, 2, 3}}
+            };
+            var sut = new MinCut(graph);
+            var result = sut.GetMinCut();
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void ShouldReadMinCutTestFileToDictionary()
+        {
+            var result = MinCut.ReadFile();
+            Assert.AreEqual(200, result.Count);
+        }
+
+        [Test]
+        public void ShouldCalcMinCutOfGivenText()
+        {
+            var sut = new MinCut(MinCut.ReadFile());
+            var result = sut.GetMinCut();
+            Assert.AreEqual(17, result);
+        }
+    }
+}
