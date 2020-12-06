0c3e00ba28a684bb0ab847036218c69b2dd3ec49
Rex Ye <rexebin@gmail.com>
2020-12-06 20:48:25 +0000

    Add Strong Components

--- GraphSearchShortestPathsDataStructures/AssignmentOne/StrongComponentTest.cs
+++ GraphSearchShortestPathsDataStructures/AssignmentOne/StrongComponentTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 
@@ -6,73 +7,248 @@ namespace GraphSearchShortestPathsDataStructures.AssignmentOne
 {
     public class StrongComponentTest
     {
-        private Dictionary<int, Vertex> _graph = null!;
+        private int[][] input =
+        {
+            new[] {1, 4},
+            new[] {2, 8},
+            new[] {3, 6},
+            new[] {4, 7},
+            new[] {5, 2},
+            new[] {6, 9},
+            new[] {7, 1},
+            new[] {8, 6},
+            new[] {8, 5},
+            new[] {9, 3},
+            new[] {9, 7}
+        };
+
         private StrongComponents _sut = null!;
+
         [SetUp]
         public void Setup()
         {
-            _graph = new Dictionary<int, Vertex>
-            {
-                {1, new Vertex(1,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
-                {2, new Vertex(2,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
-                {3, new Vertex(3,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
-                {4, new Vertex(4,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
-                {5, new Vertex(5,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
-                {6, new Vertex(6,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
-                {7, new Vertex(7,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
-                {8, new Vertex(8,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
-                {9, new Vertex(9,new List<Vertex>(), new List<Vertex>(), false, 0, null)}
-            };
-            _graph[1].Heads.Add(_graph[4]);
-            _graph[1].Tails.Add(_graph[7]);
-            _graph[2].Heads.Add(_graph[8]);
-            _graph[2].Tails.Add(_graph[5]);
-            _graph[3].Heads.Add(_graph[6]);
-            _graph[3].Tails.Add(_graph[9]);
-            _graph[4].Heads.Add(_graph[7]);
-            _graph[4].Tails.Add(_graph[1]);
-            _graph[5].Heads.Add(_graph[2]);
-            _graph[5].Tails.Add(_graph[8]);
-            _graph[6].Tails.AddRange(new List<Vertex> {_graph[3], _graph[8]});
-            _graph[6].Heads.Add(_graph[9]);
-            _graph[7].Tails.AddRange(new List<Vertex> {_graph[4], _graph[9]});
-            _graph[7].Heads.AddRange(new List<Vertex> {_graph[1]});
-            _graph[8].Tails.AddRange(new List<Vertex> {_graph[2]});
-            _graph[8].Heads.AddRange(new List<Vertex> {_graph[6], _graph[5]});
-            _graph[9].Tails.AddRange(new List<Vertex> {_graph[6]});
-            _graph[9].Heads.AddRange(new List<Vertex> {_graph[3], _graph[7]});
-            _sut = new StrongComponents(_graph);
+            var graph = StrongComponents.TransformToGraph(input);
+            _sut = new StrongComponents(graph);
         }
-        
+
+        [Test]
+        public void GivenArrayShouldGroupByFirstNumber()
+        {
+            var array = new[]
+            {
+                new[] {1, 4},
+                new[] {2, 8},
+                new[] {3, 6},
+                new[] {4, 7},
+                new[] {5, 2},
+                new[] {6, 9},
+                new[] {7, 1},
+                new[] {8, 6},
+                new[] {8, 5},
+                new[] {9, 3},
+                new[] {9, 7}
+            };
+
+            var result = StrongComponents.GroupByTails(array);
+            var expected = new Dictionary<int, int[]>()
+            {
+                {1, new[] {4}},
+                {2, new[] {8}},
+                {3, new[] {6}},
+                {4, new[] {7}},
+                {5, new[] {2}},
+                {6, new[] {9}},
+                {7, new[] {1}},
+                {8, new[] {6, 5}},
+                {9, new[] {3, 7}},
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenArrayShouldGroupByLastNumber()
+        {
+            var array = new[]
+            {
+                new[] {1, 4},
+                new[] {2, 8},
+                new[] {3, 6},
+                new[] {4, 7},
+                new[] {5, 2},
+                new[] {6, 9},
+                new[] {7, 1},
+                new[] {8, 6},
+                new[] {8, 5},
+                new[] {9, 3},
+                new[] {9, 7}
+            };
+
+            var result = StrongComponents.GroupByHeads(array);
+            var expected = new Dictionary<int, int[]>()
+            {
+                {1, new[] {7}},
+                {2, new[] {5}},
+                {3, new[] {9}},
+                {4, new[] {1}},
+                {5, new[] {8}},
+                {6, new[] {3, 8}},
+                {7, new[] {4, 9}},
+                {8, new[] {2}},
+                {9, new[] {6}},
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenArrayShouldReturnGraph()
+        {
+            var array = new[]
+            {
+                new[] {1, 4},
+                new[] {2, 8},
+                new[] {3, 6},
+                new[] {4, 7},
+                new[] {5, 2},
+                new[] {6, 9},
+                new[] {7, 1},
+                new[] {8, 6},
+                new[] {8, 5},
+                new[] {9, 3},
+                new[] {9, 7}
+            };
+
+            var graph = StrongComponents.TransformToGraph(array);
+
+            Assert.AreEqual(7, graph[1].Tails.First().OriginalNo);
+            Assert.AreEqual(4, graph[1].Heads.First().OriginalNo);
+
+            Assert.AreEqual(8, graph[2].Heads.First().OriginalNo);
+            Assert.AreEqual(5, graph[2].Tails.First().OriginalNo);
+
+            Assert.AreEqual(6, graph[3].Heads.First().OriginalNo);
+            Assert.AreEqual(9, graph[3].Tails.First().OriginalNo);
+
+            Assert.AreEqual(7, graph[4].Heads.First().OriginalNo);
+            Assert.AreEqual(1, graph[4].Tails.First().OriginalNo);
+
+            Assert.AreEqual(2, graph[5].Heads.First().OriginalNo);
+            Assert.AreEqual(8, graph[5].Tails.First().OriginalNo);
+
+            Assert.AreEqual(9, graph[6].Heads.First().OriginalNo);
+            Assert.AreEqual(3, graph[6].Tails.First().OriginalNo);
+            Assert.AreEqual(8, graph[6].Tails.Last().OriginalNo);
+
+            Assert.AreEqual(1, graph[7].Heads.First().OriginalNo);
+            Assert.AreEqual(4, graph[7].Tails.First().OriginalNo);
+
+            Assert.AreEqual(6, graph[8].Heads.First().OriginalNo);
+            Assert.AreEqual(5, graph[8].Heads.Last().OriginalNo);
+            Assert.AreEqual(2, graph[8].Tails.First().OriginalNo);
+
+            Assert.AreEqual(3, graph[9].Heads.First().OriginalNo);
+            Assert.AreEqual(7, graph[9].Heads.Last().OriginalNo);
+            Assert.AreEqual(6, graph[9].Tails.First().OriginalNo);
+
+            foreach (var vertex in graph)
+            {
+                Assert.False(vertex.Value.IsVisited);
+                Assert.Null(vertex.Value.Leader);
+                Assert.AreEqual(0, vertex.Value.FinishTime);
+                Assert.AreEqual(vertex.Key, vertex.Value.OriginalNo);
+            }
+        }
+
         [Test]
         public void GivenGroupAndLeadVertex_ShouldMarkFinishingTime()
         {
-            _sut.FirstReverseSearch();
-            Assert.AreEqual(1, _graph[3].FinishTime);
-            Assert.AreEqual(2, _graph[5].FinishTime);
-            Assert.AreEqual(3, _graph[2].FinishTime);
-            Assert.AreEqual(4, _graph[8].FinishTime);
-            Assert.AreEqual(5, _graph[6].FinishTime);
-            Assert.AreEqual(6, _graph[9].FinishTime);
-            Assert.AreEqual(7, _graph[1].FinishTime);
-            Assert.AreEqual(8, _graph[4].FinishTime);
-            Assert.AreEqual(9, _graph[7].FinishTime);
+            _sut.DepthFirstSearch(true);
+            var graph = _sut.GetGraph();
+            Assert.AreEqual(1, graph[3].FinishTime);
+            Assert.AreEqual(2, graph[5].FinishTime);
+            Assert.AreEqual(3, graph[2].FinishTime);
+            Assert.AreEqual(4, graph[8].FinishTime);
+            Assert.AreEqual(5, graph[6].FinishTime);
+            Assert.AreEqual(6, graph[9].FinishTime);
+
+            Assert.AreEqual(7, graph[1].FinishTime);
+            Assert.AreEqual(8, graph[4].FinishTime);
+            Assert.AreEqual(9, graph[7].FinishTime);
+
+            Assert.AreEqual(9, graph[3].Leader?.OriginalNo);
+            Assert.AreEqual(9, graph[5].Leader?.OriginalNo);
+            Assert.AreEqual(9, graph[2].Leader?.OriginalNo);
+            Assert.AreEqual(9, graph[8].Leader?.OriginalNo);
+            Assert.AreEqual(9, graph[6].Leader?.OriginalNo);
+            Assert.AreEqual(9, graph[9].Leader?.OriginalNo);
         }
 
         [Test]
         public void GivenFirstReverseSearchResult_ShouldSetFinishTimeToKey()
         {
-            _sut.FirstReverseSearch();
-            _sut.FinishTimeToKey();
-            Assert.AreEqual(1, _graph[7].OriginalNo);
-            Assert.AreEqual(2, _graph[3].OriginalNo);
-            Assert.AreEqual(3, _graph[1].OriginalNo);
-            Assert.AreEqual(4, _graph[8].OriginalNo);
-            Assert.AreEqual(5, _graph[2].OriginalNo);
-            Assert.AreEqual(6, _graph[5].OriginalNo);
-            Assert.AreEqual(7, _graph[9].OriginalNo);
-            Assert.AreEqual(8, _graph[4].OriginalNo);
-            Assert.AreEqual(9, _graph[6].OriginalNo);
+            _sut.DepthFirstSearch(true);
+            _sut.FinishTimeToKeyAndResetStatus();
+            var graph = _sut.GetGraph();
+            Assert.AreEqual(1, graph[7].OriginalNo);
+            Assert.AreEqual(2, graph[3].OriginalNo);
+            Assert.AreEqual(3, graph[1].OriginalNo);
+            Assert.AreEqual(4, graph[8].OriginalNo);
+            Assert.AreEqual(5, graph[2].OriginalNo);
+            Assert.AreEqual(6, graph[5].OriginalNo);
+            Assert.AreEqual(7, graph[9].OriginalNo);
+            Assert.AreEqual(8, graph[4].OriginalNo);
+            Assert.AreEqual(9, graph[6].OriginalNo);
+
+            Assert.False(graph[1].IsVisited);
+            Assert.False(graph[2].IsVisited);
+            Assert.False(graph[3].IsVisited);
+            Assert.False(graph[4].IsVisited);
+            Assert.False(graph[5].IsVisited);
+            Assert.False(graph[6].IsVisited);
+            Assert.False(graph[7].IsVisited);
+            Assert.False(graph[8].IsVisited);
+            Assert.False(graph[9].IsVisited);
+        }
+
+        [Test]
+        public void SearchProcessedGraphInCorrectOrder_ShouldSetLeaders()
+        {
+            _sut.DepthFirstSearch(true);
+            _sut.FinishTimeToKeyAndResetStatus();
+            _sut.DepthFirstSearch(false);
+            var graph = _sut.GetGraph();
+            Assert.AreEqual(7, graph[9].Leader?.OriginalNo);
+            Assert.AreEqual(7, graph[8].Leader?.OriginalNo);
+            Assert.AreEqual(7, graph[7].Leader?.OriginalNo);
+
+            Assert.AreEqual(9, graph[6].Leader?.OriginalNo);
+            Assert.AreEqual(9, graph[5].Leader?.OriginalNo);
+            Assert.AreEqual(9, graph[1].Leader?.OriginalNo);
+
+            Assert.AreEqual(8, graph[4].Leader?.OriginalNo);
+            Assert.AreEqual(8, graph[2].Leader?.OriginalNo);
+            Assert.AreEqual(8, graph[3].Leader?.OriginalNo);
+
+            var strongComponentsCounts = _sut.GetStrongComponentsCounts();
+            Assert.AreEqual(new[] {3, 3, 3}, strongComponentsCounts);
+        }
+
+        [Test]
+        public void ShouldReadSCC_ReturnGraph()
+        {
+            var graph = StrongComponents.ReadFile();
+            // Assert.AreEqual(875714, graph.Count);
+            _sut = new StrongComponents(graph);
+            _sut.DepthFirstSearch(true);
+            // graph = _sut.GetGraph();
+            // Assert.AreEqual(875714, graph.Count);
+            _sut.FinishTimeToKeyAndResetStatus();
+            // graph = _sut.GetGraph();
+            // Assert.AreEqual(875714, graph.Count);
+            _sut.DepthFirstSearch(false);
+            // graph = _sut.GetGraph();
+            // Assert.AreEqual(875714, graph.Count);
+            var result = _sut.GetStrongComponentsCounts();
+            Assert.AreEqual(new[]{1}, result);
         }
     }
 }
--- GraphSearchShortestPathsDataStructures/AssignmentOne/StrongComponents.cs
+++ GraphSearchShortestPathsDataStructures/AssignmentOne/StrongComponents.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Utility.Common;
 
 namespace GraphSearchShortestPathsDataStructures.AssignmentOne
 {
@@ -7,11 +11,12 @@ namespace GraphSearchShortestPathsDataStructures.AssignmentOne
         public int OriginalNo { get; }
         public List<Vertex> Heads { get; }
         public List<Vertex> Tails { get; }
-        public bool IsVisited { get; private set; }
+        public bool IsVisited { get; set; }
         public int FinishTime { get; set; }
-        public Vertex? Leader { get; }
+        public Vertex? Leader { get; set; }
 
-        public Vertex(int originalNo,  List<Vertex> heads, List<Vertex> tails, bool isVisited, int finishTime, Vertex? leader)
+        public Vertex(int originalNo, List<Vertex> heads, List<Vertex> tails, bool isVisited, int finishTime,
+            Vertex? leader)
         {
             OriginalNo = originalNo;
             Heads = heads;
@@ -20,53 +25,157 @@ namespace GraphSearchShortestPathsDataStructures.AssignmentOne
             FinishTime = finishTime;
             Leader = leader;
         }
-
-        public void MarkIsVisited()
-        {
-            IsVisited = true;
-        }
     };
 
     public class StrongComponents
     {
-        private readonly Dictionary<int, Vertex> _graph;
-        private int _finishingTime = 0;
+        private Dictionary<int, Vertex> graph;
+        private int _finishingTime;
         private Vertex? _lead;
 
         public StrongComponents(Dictionary<int, Vertex> graph)
         {
-            _graph = graph;
+            this.graph = graph;
         }
 
-        public void FirstReverseSearch()
+        public Dictionary<int, Vertex> GetGraph()
         {
-            for (int i = _graph.Count; i >= 1; i--)
+            return graph;
+        }
+
+        public void DepthFirstSearch(bool isReverse)
+        {
+            _finishingTime = 0;
+            _lead = null;
+            if (!isReverse)
             {
-                if (_graph[i].IsVisited)
+                foreach (var keyValuePair in graph)
+                {
+                    keyValuePair.Value.Tails.Clear();
+                    keyValuePair.Value.FinishTime = 0;
+                }
+            }
+
+            for (var i = graph.Count; i >= 1; i--)
+            {
+                if (graph[i].IsVisited)
                 {
                     continue;
                 }
-                DepthFirstSearch(_graph[i]);
+
+                _lead = graph[i];
+                DepthFirstSearch(graph[i], isReverse);
             }
         }
 
-        public void DepthFirstSearch(Vertex leadVertex)
+        private void DepthFirstSearch(Vertex vertexToSearch, bool isReverse)
         {
-            leadVertex.MarkIsVisited();
-            _lead = leadVertex;
-            foreach (var vertex in leadVertex.Tails)
+            vertexToSearch.IsVisited = true;
+            foreach (var vertex in (isReverse ? vertexToSearch.Tails : vertexToSearch.Heads))
             {
-                if (vertex.IsVisited) continue;
-                DepthFirstSearch(vertex);
+                if (vertex.IsVisited)
+                    continue;
+                DepthFirstSearch(vertex, isReverse);
             }
-
+            
+            vertexToSearch.Leader = _lead;
             _finishingTime++;
-            leadVertex.FinishTime = _finishingTime;
+            vertexToSearch.FinishTime = _finishingTime;
         }
 
-        public void FinishTimeToKey()
+        public void FinishTimeToKeyAndResetStatus()
         {
-            throw new System.NotImplementedException();
+            // var newGraph = new Dictionary<int, Vertex>();
+            // for (int i = 1; i <= graph.Count; i++)
+            // {
+            //     graph[i].IsVisited = false;
+            //     newGraph.Add(graph[i].FinishTime, graph[i]);
+            // }
+            //
+            // graph = newGraph;
+            graph = graph.ToDictionary(entry => entry.Value.FinishTime,
+                entry =>
+                {
+                    entry.Value.IsVisited = false;
+                    return entry.Value;
+                });
+        }
+
+        public int[] GetStrongComponentsCounts()
+        {
+            return graph.Select(x => x.Value)
+                .GroupBy(x => x.Leader)
+                .Select(x => x.Count())
+                .OrderByDescending(x => x)
+                .ToArray();
+        }
+
+        public static Dictionary<int, Vertex> ReadFile()
+        {
+            var array = new FileReader()
+                .ReadFile("AssignmentOne", "SCC.txt")
+                .Select(x => x.TrimEnd().Split(' ')
+                    .Select(x => int.Parse(x))
+                    .ToArray())
+                .Where(x => x[0] != x[1]);
+            var graph = TransformToGraph(array.ToArray());
+            return graph;
+        }
+
+        public static Dictionary<int, int[]> GroupByTails(int[][] array)
+        {
+            return array.GroupBy(x => x.First())
+                .ToDictionary(e => e.Key,
+                    e =>
+                        e.Select(x => x.Last()).ToArray()
+                );
+        }
+
+        public static Dictionary<int, int[]> GroupByHeads(int[][] array)
+        {
+            return array.GroupBy(x => x.Last())
+                .ToDictionary(e => e.Key,
+                    e =>
+                        e.Select(x => x.First()).ToArray()
+                );
+        }
+
+        public static Dictionary<int, Vertex> TransformToGraph(int[][] array)
+        {
+            var groupByTails = GroupByTails(array);
+            var groupByHeads = GroupByHeads(array);
+            var merged = groupByTails.ToDictionary(e => e.Key,
+                e =>
+                    new Vertex(e.Key, new List<Vertex>(), new List<Vertex>(), false, 0, null));
+            foreach (var heads in groupByHeads.Where(keyValuePair => !merged.TryGetValue(keyValuePair.Key, out _)))
+            {
+                merged.TryAdd(heads.Key,
+                    new Vertex(heads.Key, new List<Vertex>(),
+                        new List<Vertex>(), false, 0, null));
+            }
+
+            foreach (var vertex in merged)
+            {
+                if (groupByTails.TryGetValue(vertex.Key, out var allHeads))
+                {
+                    foreach (var headNo in allHeads)
+                    {
+                        if (merged.TryGetValue(headNo, out var head))
+                            vertex.Value.Heads.Add(head);
+                    }
+                }
+
+                if (groupByHeads.TryGetValue(vertex.Key, out var allTails))
+                {
+                    foreach (var tailNo in allTails)
+                    {
+                        if (merged.TryGetValue(tailNo, out var tail))
+                            vertex.Value.Tails.Add(tail);
+                    }
+                }
+            }
+
+            return merged;
         }
     }
 }
