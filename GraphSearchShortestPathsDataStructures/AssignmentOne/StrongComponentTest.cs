1694ce0196a131216a081ff5b756f9e99cca010c
Rex <rexebin@gmail.com>
2020-12-06 10:18:20 +0000

    Add Strong Components

---
+++ GraphSearchShortestPathsDataStructures/AssignmentOne/StrongComponentTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace GraphSearchShortestPathsDataStructures.AssignmentOne
+{
+    public class StrongComponentTest
+    {
+        private Dictionary<int, Vertex> _graph = null!;
+        private StrongComponents _sut = null!;
+        [SetUp]
+        public void Setup()
+        {
+            _graph = new Dictionary<int, Vertex>
+            {
+                {1, new Vertex(1,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
+                {2, new Vertex(2,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
+                {3, new Vertex(3,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
+                {4, new Vertex(4,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
+                {5, new Vertex(5,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
+                {6, new Vertex(6,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
+                {7, new Vertex(7,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
+                {8, new Vertex(8,new List<Vertex>(), new List<Vertex>(), false, 0, null)},
+                {9, new Vertex(9,new List<Vertex>(), new List<Vertex>(), false, 0, null)}
+            };
+            _graph[1].Heads.Add(_graph[4]);
+            _graph[1].Tails.Add(_graph[7]);
+            _graph[2].Heads.Add(_graph[8]);
+            _graph[2].Tails.Add(_graph[5]);
+            _graph[3].Heads.Add(_graph[6]);
+            _graph[3].Tails.Add(_graph[9]);
+            _graph[4].Heads.Add(_graph[7]);
+            _graph[4].Tails.Add(_graph[1]);
+            _graph[5].Heads.Add(_graph[2]);
+            _graph[5].Tails.Add(_graph[8]);
+            _graph[6].Tails.AddRange(new List<Vertex> {_graph[3], _graph[8]});
+            _graph[6].Heads.Add(_graph[9]);
+            _graph[7].Tails.AddRange(new List<Vertex> {_graph[4], _graph[9]});
+            _graph[7].Heads.AddRange(new List<Vertex> {_graph[1]});
+            _graph[8].Tails.AddRange(new List<Vertex> {_graph[2]});
+            _graph[8].Heads.AddRange(new List<Vertex> {_graph[6], _graph[5]});
+            _graph[9].Tails.AddRange(new List<Vertex> {_graph[6]});
+            _graph[9].Heads.AddRange(new List<Vertex> {_graph[3], _graph[7]});
+            _sut = new StrongComponents(_graph);
+        }
+        
+        [Test]
+        public void GivenGroupAndLeadVertex_ShouldMarkFinishingTime()
+        {
+            _sut.FirstReverseSearch();
+            Assert.AreEqual(1, _graph[3].FinishTime);
+            Assert.AreEqual(2, _graph[5].FinishTime);
+            Assert.AreEqual(3, _graph[2].FinishTime);
+            Assert.AreEqual(4, _graph[8].FinishTime);
+            Assert.AreEqual(5, _graph[6].FinishTime);
+            Assert.AreEqual(6, _graph[9].FinishTime);
+            Assert.AreEqual(7, _graph[1].FinishTime);
+            Assert.AreEqual(8, _graph[4].FinishTime);
+            Assert.AreEqual(9, _graph[7].FinishTime);
+        }
+
+        [Test]
+        public void GivenFirstReverseSearchResult_ShouldSetFinishTimeToKey()
+        {
+            _sut.FirstReverseSearch();
+            _sut.FinishTimeToKey();
+            Assert.AreEqual(1, _graph[7].OriginalNo);
+            Assert.AreEqual(2, _graph[3].OriginalNo);
+            Assert.AreEqual(3, _graph[1].OriginalNo);
+            Assert.AreEqual(4, _graph[8].OriginalNo);
+            Assert.AreEqual(5, _graph[2].OriginalNo);
+            Assert.AreEqual(6, _graph[5].OriginalNo);
+            Assert.AreEqual(7, _graph[9].OriginalNo);
+            Assert.AreEqual(8, _graph[4].OriginalNo);
+            Assert.AreEqual(9, _graph[6].OriginalNo);
+        }
+    }
+}
---
+++ GraphSearchShortestPathsDataStructures/AssignmentOne/StrongComponents.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace GraphSearchShortestPathsDataStructures.AssignmentOne
+{
+    public class Vertex
+    {
+        public int OriginalNo { get; }
+        public List<Vertex> Heads { get; }
+        public List<Vertex> Tails { get; }
+        public bool IsVisited { get; private set; }
+        public int FinishTime { get; set; }
+        public Vertex? Leader { get; }
+
+        public Vertex(int originalNo,  List<Vertex> heads, List<Vertex> tails, bool isVisited, int finishTime, Vertex? leader)
+        {
+            OriginalNo = originalNo;
+            Heads = heads;
+            Tails = tails;
+            IsVisited = isVisited;
+            FinishTime = finishTime;
+            Leader = leader;
+        }
+
+        public void MarkIsVisited()
+        {
+            IsVisited = true;
+        }
+    };
+
+    public class StrongComponents
+    {
+        private readonly Dictionary<int, Vertex> _graph;
+        private int _finishingTime = 0;
+        private Vertex? _lead;
+
+        public StrongComponents(Dictionary<int, Vertex> graph)
+        {
+            _graph = graph;
+        }
+
+        public void FirstReverseSearch()
+        {
+            for (int i = _graph.Count; i >= 1; i--)
+            {
+                if (_graph[i].IsVisited)
+                {
+                    continue;
+                }
+                DepthFirstSearch(_graph[i]);
+            }
+        }
+
+        public void DepthFirstSearch(Vertex leadVertex)
+        {
+            leadVertex.MarkIsVisited();
+            _lead = leadVertex;
+            foreach (var vertex in leadVertex.Tails)
+            {
+                if (vertex.IsVisited) continue;
+                DepthFirstSearch(vertex);
+            }
+
+            _finishingTime++;
+            leadVertex.FinishTime = _finishingTime;
+        }
+
+        public void FinishTimeToKey()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
