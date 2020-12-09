c19f28e3ef89a3cef30d5252caf4d0d6a2df63e6
Rex <rexebin@gmail.com>
2020-12-09 13:28:17 +0000

    Add Heap Data Structure.

--- Utility/DataStructures/Heap.cs
+++ Utility/DataStructures/Heap.cs
@@ -1,7 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Utility.DataStructures
 {
-    public class Heap
+    public interface IHeap<T> where T : Record, IComparable<T>
     {
-        
+        void Insert(T item);
+        T? Pull();
+        void InsertMany(IEnumerable<T> records);
+        bool TryDelete(Predicate<T> predicate);
+    }
+
+    public class Heap<T> : IHeap<T> where T : Record, IComparable<T>
+    {
+        private readonly List<T> _backingList = new();
+
+        public List<T> ToArray()
+        {
+            return _backingList;
+        }
+
+        public Heap()
+        {
+        }
+
+        public Heap(IEnumerable<T> seeds)
+        {
+            InsertMany(seeds);
+        }
+
+        public void Insert(T item)
+        {
+            _backingList.Add(item with {});
+            BubbleUp(_backingList.Count - 1);
+        }
+
+        private void BubbleUp(int index)
+        {
+            while (true)
+            {
+                var parent = GetParentIndex(index);
+                if (_backingList[parent].CompareTo(_backingList[index]) != 1) return;
+                Swap(index, parent);
+                index = parent;
+            }
+        }
+
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        public T? Pull()
+        {
+            if (!_backingList.Any()) return null;
+            var top = _backingList[0];
+            Swap(0, _backingList.Count - 1);
+            _backingList.RemoveAt(_backingList.Count - 1);
+            BubbleDown(0);
+            return top;
+        }
+
+        private void BubbleDown(int i)
+        {
+            while (true)
+            {
+                var leftChild = GetLeftChild(i);
+                var rightChild = GetRightChild(i);
+                var smallerChild = GetSmallerChildIndex(leftChild, rightChild);
+                if (smallerChild == null) return;
+                if (_backingList[i].CompareTo(_backingList[smallerChild.Value]) != 1) return;
+                Swap(i, smallerChild.Value);
+                i = smallerChild.Value;
+            }
+        }
+
+        private int? GetSmallerChildIndex(int? leftChild, int? rightChild)
+        {
+            return leftChild != null && rightChild != null
+                ? _backingList[leftChild.Value].CompareTo(_backingList[rightChild.Value]) == -1
+                    ? leftChild
+                    : rightChild
+                : leftChild;
+        }
+
+        private void Swap(int index1, int index2)
+        {
+            var buff = _backingList[index1];
+            _backingList[index1] = _backingList[index2];
+            _backingList[index2] = buff;
+        }
+
+        private int? GetRightChild(int i)
+        {
+            var index = i * 2 + 2;
+            if (index < _backingList.Count) return index;
+            return null;
+        }
+
+        private int? GetLeftChild(int i)
+        {
+            var index = i * 2 + 1;
+            if (index < _backingList.Count) return index;
+            return null;
+        }
+
+        public void InsertMany(IEnumerable<T> records)
+        {
+            foreach (var testRecord in records) Insert(testRecord);
+        }
+
+        public bool TryDelete(Predicate<T> predicate)
+        {
+            var itemToDelete = _backingList.Find(predicate);
+            if (itemToDelete == null)
+                return false;
+
+            var index = _backingList.IndexOf(itemToDelete);
+            Swap(index, _backingList.Count - 1);
+            _backingList.RemoveAt(_backingList.Count - 1);
+            BubbleDown(index);
+            return true;
+        }
     }
 }
--- Utility/DataStructures/HeapTest.cs
+++ Utility/DataStructures/HeapTest.cs
@@ -1,7 +1,215 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
 namespace Utility.DataStructures
 {
+    public abstract record Record;
+
+    public record TestRecord(int Id, int Length) : Record, IComparable<TestRecord>
+    {
+        public int CompareTo(TestRecord? record)
+        {
+            if (ReferenceEquals(this, record)) return 0;
+            if (record == null) return 1;
+            if (Length < record.Length) return -1;
+            if (Length > record.Length) return 1;
+            return 0;
+        }
+    }
+
     public class HeapTest
     {
-        
+        [Test]
+        public void GivenElement_ShouldAddToHeap()
+        {
+            var heap = new Heap<TestRecord>();
+            var record = new TestRecord(1, 2);
+            heap.Insert(record);
+            Assert.AreEqual(new[] {record}, heap.ToArray());
+        }
+
+        [Test]
+        public void InsertElement_ToExistingHeap_ShouldInsertAndMaintainHeapStructure()
+        {
+            var heap = new Heap<TestRecord>();
+            var record1 = new TestRecord(1, 2);
+            var record2 = new TestRecord(2, 1);
+            heap.Insert(record1);
+            heap.Insert(record2);
+            Assert.AreEqual(new[] {record2 with{}, record1 with{}}, heap.ToArray());
+        }
+
+        [Test]
+        public void Pull_ShouldRemoveRootElementAndMaintainHeap()
+        {
+            var heap = new Heap<TestRecord>();
+            var record1 = new TestRecord(1, 2);
+            var record2 = new TestRecord(2, 1);
+            var record3 = new TestRecord(3, 3);
+            heap.Insert(record1);
+            heap.Insert(record2);
+            heap.Insert(record3);
+            var record = heap.Pull();
+            Assert.AreEqual(new TestRecord(2, 1), record);
+            Assert.AreEqual(new List<TestRecord> {new TestRecord(1, 2), new TestRecord(3, 3)}, heap.ToArray());
+        }
+
+        [Test]
+        public void PullOnEmptyHeapShouldReturnNull()
+        {
+            var heap = new Heap<TestRecord>();
+            var min = heap.Pull();
+            Assert.Null(min);
+        }
+
+        [Test]
+        public void GivenCollectionOfElements_ShouldHeapify()
+        {
+            var records = new List<TestRecord>
+            {
+                new(1, 5), new(2, 3), new(3, 4)
+            };
+            var heap = new Heap<TestRecord>();
+            heap.InsertMany(records);
+            Assert.AreEqual(new List<TestRecord>
+            {
+                new(2, 3), new(1, 5), new(3, 4),
+            }, heap.ToArray());
+        }
+
+        [Test]
+        public void ShouldDeleteElementFromHeap()
+        {
+            var records = new List<TestRecord>
+            {
+                new(1, 5), new(2, 3), new(3, 4)
+            };
+            var heap = new Heap<TestRecord>();
+            heap.InsertMany(records);
+            var result = heap.TryDelete(record => record.Id == 3);
+            Assert.True(result);
+            Assert.AreEqual(new List<TestRecord>
+            {
+                new(2, 3), new(1, 5)
+            }, heap.ToArray());
+        }
+
+        [Test]
+        public void DeleteNonExisitItemShouldReturnFalse()
+        {
+            var records = new List<TestRecord>
+            {
+                new(1, 5), new(2, 3), new(3, 4)
+            };
+            var heap = new Heap<TestRecord>();
+            heap.InsertMany(records);
+            var result = heap.TryDelete(record => record.Id == 4);
+            Assert.False(result);
+            Assert.AreEqual(new List<TestRecord>
+            {
+                new(2, 3), new(1, 5), new(3, 4)
+            }, heap.ToArray());
+        }
+
+        [Test]
+        public void ShouldHeapifyLargerCollection()
+        {
+            var records = new List<TestRecord>
+            {
+                new(8, 8),
+                new(4, 4),
+                new(5, 5),
+                new(9, 9),
+                new(12, 12),
+                new(15, 15),
+                new(3, 3),
+                new(1, 1),
+                new(2, 2),
+                new(6, 6),
+                new(7, 7),
+                new(7, 7)
+            };
+            var heap = new Heap<TestRecord>(records);
+
+            Assert.AreEqual(new List<TestRecord>
+            {
+                new(1, 1),
+                new(2, 2),
+                new(4, 4),
+                new(3, 3),
+                new(6, 6),
+                new(7, 7),
+                new(5, 5),
+                new(9, 9),
+                new(8, 8),
+                new(12, 12),
+                new(7, 7),
+                new(15, 15),
+            }, heap.ToArray());
+            heap.TryDelete(x => x.Id == 3);
+            Assert.AreEqual(new List<TestRecord>
+            {
+                new(1, 1),
+                new(2, 2),
+                new(4, 4),
+                new(8, 8),
+                new(6, 6),
+                new(7, 7),
+                new(5, 5),
+                new(9, 9),
+                new(15, 15),
+                new(12, 12),
+                new(7, 7),
+            }, heap.ToArray());
+            var min = heap.Pull();
+            Assert.AreEqual(new TestRecord(1, 1), min);
+            Assert.AreEqual(new List<TestRecord>
+            {
+                new(2, 2),
+                new(6, 6),
+                new(4, 4),
+                new(8, 8),
+                new(7, 7),
+                new(7, 7),
+                new(5, 5),
+                new(9, 9),
+                new(15, 15),
+                new(12, 12),
+            }, heap.ToArray());
+            heap.Insert(new TestRecord(15, 9));
+            Assert.AreEqual(new List<TestRecord>
+            {
+                new(2, 2),
+                new(6, 6),
+                new(4, 4),
+                new(8, 8),
+                new(7, 7),
+                new(7, 7),
+                new(5, 5),
+                new(9, 9),
+                new(15, 15),
+                new(12, 12),
+                new(15, 9)
+            }, heap.ToArray());
+            heap.Insert(new TestRecord(15, 1));
+            Assert.AreEqual(new List<TestRecord>
+            {
+                new TestRecord(15, 1),
+                new(6, 6),
+                new(2, 2),
+                new(8, 8),
+                new(7, 7),
+                new(4, 4),
+                new(5, 5),
+                new(9, 9),
+                new(15, 15),
+                new(12, 12),
+                new(15, 9),
+                new(7, 7),
+            }, heap.ToArray());
+        }
     }
 }
