15b62cb51e4467b945dece01af9cd627ad6c6f25
Rex <rexebin@gmail.com>
2020-12-12 11:17:34 +0000

    Add Two Sums

---
+++ GraphSearchShortestPathsDataStructures/AssignmentFour/TwoSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphSearchShortestPathsDataStructures.AssignmentFour
+{
+    public class TwoSum
+    {
+        public readonly Dictionary<long, long[]> Input;
+        public readonly HashSet<long> HasTwoSum = new();
+
+        public TwoSum(long[] input)
+        {
+            Input = input.GroupBy(x => Math.Abs(x) / 10000)
+                .Where(x => x.Count() >= 2)
+                .ToDictionary(x => x.Key,
+                    e => e.ToArray());
+        }
+
+        public void GetCount(long[] input)
+        {
+            for (var index = 0; index < input.Length; index++)
+            {
+                var v = input[index];
+                for (var i = 0; i < input.Length; i++)
+                {
+                    if (index == i) continue;
+                    var v1 = input[i];
+                    var sum = v + v1;
+                    if (sum >= -10000 && sum <= 10000)
+                    {
+                        HasTwoSum.Add(sum);
+                    }
+                }
+            }
+        }
+
+        public int GetAllCount()
+        {
+            foreach (var i in Input)
+            {
+                GetCount(i.Value);
+            }
+
+            return HasTwoSum.Count;
+        }
+    }
+}
---
+++ GraphSearchShortestPathsDataStructures/AssignmentFour/TwoSumTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using Utility.Common;
+
+namespace GraphSearchShortestPathsDataStructures.AssignmentFour
+{
+    public class TwoSumTest
+    {
+        private readonly long[] _input = new[]
+        {
+            68037543430,
+            68037543431,
+            -68037543123,
+            0,
+            -10000,
+            15493,
+            5000,
+            5000
+        };
+        
+        private Dictionary<long, long[]> _dictionary = new Dictionary<long, long[]>
+        {
+            {6803754, new[] {68037543430, 68037543431, -68037543123}},
+            {0, new[] {0L, 5000L, 5000L}},
+            {1, new[] {-10000L, 15493L}},
+        };
+        
+        [Test]
+        public void GivenTestArray_ShouldBucketThemIntoTenThousands()
+        {
+            var sut = new TwoSum(_input);
+            Assert.AreEqual(_dictionary, sut.Input);
+        }
+        
+        [Test]
+        public void GivenGroupedDict_ShouldCountTwoSumBetween_Negative10000_10000()
+        {
+            var sut = new TwoSum(_input);
+            sut.GetAllCount();
+        
+            Assert.AreEqual(new HashSet<int> {307, 308, 5000, 10000, 5493},
+                sut.HasTwoSum);
+        }
+        
+        [Test]
+        public void GivenTestInput_ShouldReturnCount5()
+        {
+            var sut = new TwoSum(_input);
+            Assert.AreEqual(5, sut.GetAllCount());
+        }
+
+        [Test]
+        public void GivenTestArray_ShouldCountTwoSumCounts()
+        {
+            var input = new FileReader().ReadFile("AssignmentFour", "2sum.txt")
+                .Select(long.Parse);
+            var sut = new TwoSum(input.ToArray());
+            Assert.AreEqual(414, sut.GetAllCount());
+        }
+    }
+}
