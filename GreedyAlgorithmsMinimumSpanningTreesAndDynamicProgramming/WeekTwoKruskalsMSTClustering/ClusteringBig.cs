b892a5ef25c36c65e4addbddcca86851ee50cce1
Rex <rexebin@gmail.com>
2020-12-17 17:07:40 +0000

    Add ClusteringBig

---
+++ GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming/WeekTwoKruskalsMSTClustering/ClusteringBig.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utility.Common;
+using Utility.UnionFind;
+
+namespace GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming.WeekTwoKruskalsMSTClustering
+{
+    public class ClusteringBig
+    {
+        public List<string> GetCombinations(
+            char[] characters, int distance, int startIndex = 0)
+        {
+            var combinations = new List<string>();
+            for (var i = startIndex; i <= characters.Length - distance; i++)
+            {
+                characters[i] = Toggle(characters[i]);
+                if (distance == 1)
+                    combinations.Add(new string(characters));
+                else
+                    combinations.AddRange(GetCombinations(characters, distance - 1, i + 1));
+
+                characters[i] = Toggle(characters[i]);
+            }
+
+            return combinations;
+        }
+
+        private char Toggle(char c)
+        {
+            return c == '0' ? '1' : '0';
+        }
+
+        public HashSet<string> ReadFile(string filename)
+        {
+            var content = new FileReader().ReadFile(filename).Skip(1).Select(x => x.TrimEnd().Replace(" ", ""));
+            return content.ToHashSet();
+        }
+
+        public Dictionary<string, UnionFindNode<string>> GetInitialClusters(HashSet<string> graph)
+        {
+            return graph.ToDictionary(e => e, UnionFindNode<string>.Parse);
+        }
+
+        public int GetClusterQuantity(string filename, int distance)
+        {
+            var graph = ReadFile(filename);
+            var clusters = GetInitialClusters(graph);
+            var clustersCount = clusters.Count;
+            foreach (var vertex1 in clusters)
+            {
+                var nodes = GetNodesWithDistanceLessThanThree(vertex1.Key.ToCharArray(), graph);
+                foreach (var node in nodes)
+                {
+                    var vertex = clusters[node];
+                    if (vertex1.Value.IsSameUnion(vertex))
+                        continue;
+
+                    vertex1.Value.Union(vertex);
+                    clustersCount--;
+                }
+            }
+
+            return clustersCount;
+        }
+
+        private List<string> GetNodesWithDistanceLessThanThree(char[] input, HashSet<string> graph)
+        {
+            var combinations = new List<string>();
+            for (int i = 1; i < 3; i++)
+            {
+                combinations.AddRange(GetCombinations(input, i).Where(graph.Contains));
+            }
+
+            return combinations;
+        }
+    }
+}
---
+++ GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming/WeekTwoKruskalsMSTClustering/ClusteringBigTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Utility.UnionFind;
+
+namespace GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming.WeekTwoKruskalsMSTClustering
+{
+    public class ClusteringBigTest
+    {
+        [Test]
+        public void GivenOneBitAndDistanceOne_ShouldGetOneCombination()
+        {
+            var input = "1".ToCharArray();
+            var sut = new ClusteringBig();
+            Assert.AreEqual("0", sut.GetCombinations(input, 1).Single());
+            input = "0".ToCharArray();
+            Assert.AreEqual("1", sut.GetCombinations(input, 1).Single());
+        }
+
+        [Test]
+        public void GivenTwoBitAndDistanceOne_ShouldReturnTwoCombinations()
+        {
+            var input = "00".ToCharArray();
+            var sut = new ClusteringBig();
+            Assert.AreEqual(new List<string> {"10", "01"}, sut.GetCombinations(input, 1));
+            input = "11".ToCharArray();
+            Assert.AreEqual(new List<string> {"01", "10"}, sut.GetCombinations(input, 1));
+            input = "01".ToCharArray();
+            Assert.AreEqual(new List<string> {"11", "00"}, sut.GetCombinations(input, 1));
+            input = "10".ToCharArray();
+            Assert.AreEqual(new List<string> {"00", "11"}, sut.GetCombinations(input, 1));
+        }
+
+        [Test]
+        public void GivenTwoBitAndDistanceTwo_ShouldReturnTwoCombinations()
+        {
+            var input = "00".ToCharArray();
+            var sut = new ClusteringBig();
+            Assert.AreEqual(new List<string> {"11"}, sut.GetCombinations(input, 2));
+            input = "11".ToCharArray();
+            Assert.AreEqual(new List<string> {"00"}, sut.GetCombinations(input, 2));
+            input = "01".ToCharArray();
+            Assert.AreEqual(new List<string> {"10"}, sut.GetCombinations(input, 2));
+            input = "10".ToCharArray();
+            Assert.AreEqual(new List<string> {"01"}, sut.GetCombinations(input, 2));
+        }
+
+        [Test]
+        public void GivenThreeBitAndDistanceTwo_ShouldReturnTwoCombinations()
+        {
+            var input = "000".ToCharArray();
+            var sut = new ClusteringBig();
+            Assert.AreEqual(new List<string> {"110", "101", "011"}, sut.GetCombinations(input, 2));
+            input = "111".ToCharArray();
+            Assert.AreEqual(new List<string> {"001", "010", "100"}, sut.GetCombinations(input, 2));
+            input = "010".ToCharArray();
+            Assert.AreEqual(new List<string> {"100", "111", "001"}, sut.GetCombinations(input, 2));
+            input = "100".ToCharArray();
+            Assert.AreEqual(new List<string> {"010", "001", "111"}, sut.GetCombinations(input, 2));
+        }
+
+        [Test]
+        public void ShouldReadFile_ParseToUniqueString()
+        {
+            var sut = new ClusteringBig();
+            var graph = sut.ReadFile("clustering_big.txt");
+            Assert.AreEqual("111000001101001111001111", graph.First());
+        }
+
+        [Test]
+        public void ShouldInitializeClustersPerNode()
+        {
+            var sut = new ClusteringBig();
+            var graph = sut.ReadFile("clustering_big.txt");
+            var clusters = sut.GetInitialClusters(graph);
+            Assert.AreEqual(graph.Count, clusters.Count);
+            Assert.AreEqual("111000001101001111001111", clusters["111000001101001111001111"].Value);
+        }
+
+        [Test]
+        public void ShouldCalculateClusterQuantity()
+        {
+            var sut = new ClusteringBig();
+            var quantity = sut.GetClusterQuantity("clustering_big.txt", 3);
+            Assert.AreEqual(6118, quantity);
+        }
+    }
+}
