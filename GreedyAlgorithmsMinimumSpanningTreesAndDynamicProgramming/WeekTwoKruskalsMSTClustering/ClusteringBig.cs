7e08c35b18a188f2d20e2ce528ce836454d2d977
Rex <rexebin@gmail.com>
2020-12-17 17:13:14 +0000

    Add ClusteringBig

--- GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming/WeekTwoKruskalsMSTClustering/ClusteringBig.cs
+++ GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming/WeekTwoKruskalsMSTClustering/ClusteringBig.cs
@@ -8,6 +8,35 @@ namespace GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming.WeekTwoKrusk
 {
     public class ClusteringBig
     {
+        public int GetClusterQuantity(string filename, int distance)
+        {
+            var graph = ReadFile(filename);
+            var clusters = GetInitialClusters(graph);
+            var clustersCount = clusters.Count;
+            foreach (var vertex1 in clusters)
+            {
+                var nodes = GetNodesWithDistanceLessThanThree(vertex1.Key.ToCharArray(), graph);
+                foreach (var node in nodes)
+                {
+                    var vertex = clusters[node];
+                    if (vertex1.Value.IsSameUnion(vertex))
+                        continue;
+
+                    vertex1.Value.Union(vertex);
+                    clustersCount--;
+                }
+            }
+
+            return clustersCount;
+        }
+
+        private List<string> GetNodesWithDistanceLessThanThree(char[] input, HashSet<string> graph)
+        {
+            var combinations = new List<string>();
+            for (int i = 1; i < 3; i++) combinations.AddRange(GetCombinations(input, i).Where(graph.Contains));
+            return combinations;
+        }
+
         public List<string> GetCombinations(
             char[] characters, int distance, int startIndex = 0)
         {
@@ -40,39 +69,6 @@ namespace GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming.WeekTwoKrusk
         public Dictionary<string, UnionFindNode<string>> GetInitialClusters(HashSet<string> graph)
         {
             return graph.ToDictionary(e => e, UnionFindNode<string>.Parse);
-        }
-
-        public int GetClusterQuantity(string filename, int distance)
-        {
-            var graph = ReadFile(filename);
-            var clusters = GetInitialClusters(graph);
-            var clustersCount = clusters.Count;
-            foreach (var vertex1 in clusters)
-            {
-                var nodes = GetNodesWithDistanceLessThanThree(vertex1.Key.ToCharArray(), graph);
-                foreach (var node in nodes)
-                {
-                    var vertex = clusters[node];
-                    if (vertex1.Value.IsSameUnion(vertex))
-                        continue;
-
-                    vertex1.Value.Union(vertex);
-                    clustersCount--;
-                }
-            }
-
-            return clustersCount;
-        }
-
-        private List<string> GetNodesWithDistanceLessThanThree(char[] input, HashSet<string> graph)
-        {
-            var combinations = new List<string>();
-            for (int i = 1; i < 3; i++)
-            {
-                combinations.AddRange(GetCombinations(input, i).Where(graph.Contains));
-            }
-
-            return combinations;
         }
     }
 }
