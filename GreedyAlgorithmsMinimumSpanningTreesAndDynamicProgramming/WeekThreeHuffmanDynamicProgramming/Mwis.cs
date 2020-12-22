40283969b94427e52525596006f24527a692512d
Rex Ye <rexebin@gmail.com>
2020-12-22 21:37:57 +0000

    Add Mwis

---
+++ GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming/WeekThreeHuffmanDynamicProgramming/Mwis.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utility.Common;
+
+namespace GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming.WeekThreeHuffmanDynamicProgramming
+{
+    public class Mwis
+    {
+        public List<int> Weights { get; }
+        private List<int> MaxWeights { get; set; } = new();
+
+        public Mwis(List<int> weights)
+        {
+            Weights = weights;
+        }
+
+        public string GetAnswer()
+        {
+            var maxIndependentWeight = GetMaxIndependentWeight();
+            var requestedIndexes = new[] {1, 2, 3, 4, 17, 117, 517, 997};
+            return string.Join("",
+                requestedIndexes.Select(x =>
+                    maxIndependentWeight.TryGetValue(x, out _) ? "1" : "0"));
+        }
+
+        public Dictionary<int, int> GetMaxIndependentWeight()
+        {
+            MaxWeights = GetMaxWeights();
+            var maxWeightIndependentSet = new Dictionary<int, int>();
+            var i = MaxWeights.Count - 1;
+            while (i >= 1)
+            {
+                if (MaxWeights[i] > MaxWeights[i - 1])
+                {
+                    maxWeightIndependentSet.Add(i, Weights[i - 1]);
+                    i -= 2;
+                    continue;
+                }
+
+                i -= 1;
+            }
+
+            return maxWeightIndependentSet;
+        }
+
+        public List<int> GetMaxWeights()
+        {
+            var result = new List<int> {0, Weights[0]};
+            for (var i = 1; i < Weights.Count; i++)
+                result.Add(Math.Max(result[i], result[i - 1] + Weights[i]));
+            return result;
+        }
+
+        public static List<int> ReadFile(string filename)
+        {
+            return new FileReader().ReadFile(filename).Skip(1).Select(int.Parse).ToList();
+        }
+    }
+}
---
+++ GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming/WeekThreeHuffmanDynamicProgramming/MwisTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace GreedyAlgorithmsMinimumSpanningTreesAndDynamicProgramming.WeekThreeHuffmanDynamicProgramming
+{
+    public class MwisTest
+    {
+        [Test]
+        public void ShouldGoThroughVertices_AndMemorizeMaxWeight()
+        {
+            var sut = new Mwis
+                (new List<int> {1, 4, 1, 1, 3, 7, 8, 9, 9, 5, 4, 2});
+            var expected = new List<int> {0, 1, 4, 4, 5, 7, 12, 15, 21, 24, 26, 28, 28};
+            // 28 = 2+5+9+7+1+4
+            // 28 = 4+9+8+3+4
+            Assert.AreEqual(expected, sut.GetMaxWeights());
+        }
+
+        [Test]
+        public void Should_GetMaxIndependentWeights()
+        {
+            var sut = new Mwis
+                (new List<int> {1, 4, 1, 1, 3, 7, 8, 9, 9, 5, 4, 2});
+            var result = sut.GetMaxIndependentWeight();
+            var expected = new Dictionary<int, int>
+            {
+                {11, 4}, {9, 9}, {7, 8}, {5, 3}, {2, 4}
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ShouldReadFileToArray()
+        {
+            var sut = new Mwis(ReadFile());
+            Assert.AreEqual(1000, sut.Weights.Count);
+        }
+
+        private static List<int> ReadFile()
+        {
+            return Mwis.ReadFile("Mwis.txt");
+        }
+
+        [Test]
+        public void ShouldReturnAnswer()
+        {
+            var sut = new Mwis(ReadFile());
+            var answer = sut.GetAnswer();
+            var expected = "10100110";
+            Assert.AreEqual(expected, answer);
+        }
+    }
+}
